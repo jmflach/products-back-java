cc961cd6122bda4d8f82bd86e223b6c886e9d541
João Flach <joaoflach@gmail.com>
2023-08-09 18:14:27 -0300

    database migration working

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,42 @@
+package com.example.springboot.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable{
+    private static final long serialVersionID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+    public String getName() {
+        return name;
+    }
+    public BigDecimal getValue() {
+        return value;
+    }
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
